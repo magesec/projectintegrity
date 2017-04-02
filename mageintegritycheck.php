79b066be8862e9f25171859fd079fc0469ebd6bb
Martin Pachol <martin@magemojo.com>
2017-04-02 10:18:17 -0700

    alpha 1

--- mageintegritycheck.php
+++ mageintegritycheck.php
@@ -1,16 +1,28 @@
 <?php
 $checkpath = $argv[1];
+$checksumurl = 'https://github.com/magesec/projectintegrity/raw/master/checksums/';
 
-if (!file_exists($checkfile)) {
+
+
+if (!file_exists($checkpath)) {
   echo "Filepath not found!";
 }
 
-if (!file_exists($checkfile."/app/Mage.php")) {
+if (!file_exists($checkpath."/app/Mage.php")) {
   echo "Could not find Mage.php, is this a magento install?";
 } else {
-  require $checkfile."/app/Mage.php";
+  require $checkpath."/app/Mage.php";
   $version =  Mage::getVersion();
 }
 
+$json = file_get_contents($checksumurl.'checksum-'.$version.'.json');
 
+$checks = json_decode($json);
+
+foreach ($checks as $file=>$goodsha) {
+  $filesha = sha1_file($checkpath."/".$file);
+  if ($filesha != $goodsha) {
+    print $file.' has been modified.'."\n";
+  }
+}
 ?>

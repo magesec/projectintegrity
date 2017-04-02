568d7eb8a96b49ad5fa26f24fa99101ee32fd4cf
Martin Pachol <martin@magemojo.com>
2017-04-02 10:40:16 -0700

    alpha 2

--- mageintegritycheck.php
+++ mageintegritycheck.php
@@ -6,13 +6,28 @@ $checksumurl = 'https://github.com/magesec/projectintegrity/raw/master/checksums
 
 if (!file_exists($checkpath)) {
   echo "Filepath not found!";
+  exit;
 }
 
-if (!file_exists($checkpath."/app/Mage.php")) {
-  echo "Could not find Mage.php, is this a magento install?";
+if (file_exists($checkpath."app/etc/local.xml")) {
+  #Should be a magento 1 install
+  if (!file_exists($checkpath."/app/Mage.php")) {
+    echo "Could not find Mage.php, is this a magento install?\n";
+    exit;
+  } else {
+    require $checkpath."/app/Mage.php";
+    $version = Mage::getVersion();
+  }
+} else if (file_exists($checkpath."app/etc/env.php")) {
+  if (!file_exists($checkpath."/app/bootstrap.php")) {
+    echo "Could not find Mage.php, is this a magento install?\n";
+    exit;
+  } else {
+    require $checkpath."/app/bootstrap.php";
+    $version = \Magento\Framework\AppInterface::VERSION;
+  }
 } else {
-  require $checkpath."/app/Mage.php";
-  $version =  Mage::getVersion();
+  echo "Could not determine Magento major version, is this a magento install?\n";
 }
 
 $json = file_get_contents($checksumurl.'checksum-'.$version.'.json');
